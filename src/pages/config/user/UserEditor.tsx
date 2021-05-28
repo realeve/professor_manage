f9680d0ffed27f0bbde11c444ed41b9a6416fa37
realeve <realeve@qq.com>
2021-05-28 10:59:40 +0800

    个人基础信息编辑

--- src/pages/config/user/UserEditor.tsx
+++ src/pages/config/user/UserEditor.tsx
@@ -6,6 +6,8 @@ import { IUserItem } from '../tags/db';
 import { userBaseInfo } from '@/pages/excel/db';
 import 'antd/lib/form/style/index.less';
 import './userEdit.less';
+import { Button, message } from 'antd';
+import * as db from './db';
 
 export default ({ user }: { user: IUserItem }) => {
   const [state, setState] = useSetState(user);
@@ -14,11 +16,11 @@ export default ({ user }: { user: IUserItem }) => {
   }, [user]);
 
   return (
-    <div>
+    <>
       <h3>基础信息</h3>
       <div className="ant-form ant-form-horizontal">
         {userBaseInfo.map(({ key, value }) => (
-          <FormItem label={value} name={key} key={key}>
+          <FormItem label={value} name={key} key={key} style={{ marginBottom: 10 }}>
             <Input
               value={state[key]}
               onChange={(e) => {
@@ -29,6 +31,28 @@ export default ({ user }: { user: IUserItem }) => {
           </FormItem>
         ))}
       </div>
-    </div>
+      <div style={{ marginLeft: 150 }}>
+        <Button
+          type="primary"
+          onClick={() => {
+            db.setProfessorUser(state).then((success) => {
+              message[success ? 'success' : 'error'](`个人信息更新${success ? '成功' : '失败'}`);
+            });
+          }}
+        >
+          更新基础信息
+        </Button>
+        <Button
+          type="default" style={{marginLeft:20}}
+          onClick={() => {
+            db.addProfessorUser(state).then((success) => {
+              message[success ? 'success' : 'error'](`个人信息增加${success ? '成功' : '失败'}`);
+            });
+          }}
+        >
+          新增专家
+        </Button>
+      </div>
+    </>
   );
 };
---
+++ src/pages/config/user/db.tsx
@@ -0,0 +1,81 @@
+import { axios, TDbWrite,_commonData,DEV } from '@/utils/axios';
+
+
+
+
+/**
+ *   @database: { 接口管理 }
+ *   @desc:     { 更新专家基础信息 }  
+ */
+ export const setProfessorUser: (params: {
+    mark_label: string;
+    username: string;
+    sex: string;
+    people: string;
+    birth_date: string;
+    degree: string;
+    graduate_date: string;
+    educate_background: string;
+    hometown: string;
+    card_type: string;
+    card_no: string;
+    career: string;
+    politial_status: string;
+    company: string;
+    work_time_start: string;
+    company_type: string;
+    duty: string;
+    tech_level: string;
+    work_status: string;
+    industry_type: string;
+    vocate_qualify: string;
+    email: string;
+    phone: string;
+    mobile: string;
+    uid: string;
+  }) => Promise < boolean > = params => axios < TDbWrite > ({
+    url: DEV ? _commonData : '/1351/4cd4b091cf.json',
+    params,
+  }).then(({
+    data: [{
+      affected_rows
+    }]
+  }) => affected_rows > 0);
+
+  /**
+ *   @database: { 接口管理 }
+ *   @desc:     { 新增专家 }  
+ */
+export const addProfessorUser: (params: {
+    mark_label: string;
+    username: string;
+    sex: string;
+    people: string;
+    birth_date: string;
+    degree: string;
+    graduate_date: string;
+    educate_background: string;
+    hometown: string;
+    card_type: string;
+    card_no: string;
+    career: string;
+    politial_status: string;
+    company: string;
+    work_time_start: string;
+    company_type: string;
+    duty: string;
+    tech_level: string;
+    work_status: string;
+    industry_type: string;
+    vocate_qualify: string;
+    email: string;
+    phone: string;
+    mobile: string;
+  }) => Promise < number > = params => axios < TDbWrite > ({
+    url: DEV ? _commonData : '/1352/2c66a09c55.json',
+    params,
+  }).then(({
+    data: [{
+      id
+    }]
+  }) => id);
